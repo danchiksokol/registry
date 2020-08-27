53bf7057d14b47f19a6605d7479a4b6ebd4a434b
Daniel Sokolov <d.sokolov@roseltorg.ru>
2020-08-28 01:55:43 +0300

    Сделал indexController

---
+++ app/src/Controller/SearchController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    /**
+     * @Route("/search", name="search")
+     */
+    public function index()
+    {
+        return $this->render(
+            'search/index.html.twig',
+            [
+                'controller_name' => 'SearchController',
+            ]
+        );
+    }
+
+    public function searchBar()
+    {
+        $form = $this->createFormBuilder(null)->add('query', TextType::class)
+            ->add(
+                'search',
+                SubmitType::class,
+                ['attr' => ['class' => 'btn btn-ptymary']]
+            )->getForm();
+
+        return $this->render(
+            'search/searchBar.html.twig',
+            ['form' => $form->createView()]
+        );
+    }
+}
